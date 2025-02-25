a11489402286a17e28f17ebfb50000033bf1bd84
poojamokal <mokalpooja380@gmail.com>
2025-02-25 23:25:46 +0530

    second commit

--- lib/wet_areas_screen.dart
+++ lib/wet_areas_screen.dart
@@ -76,6 +76,7 @@ class WetAreasScreen extends StatelessWidget {
                             },
                           ),
                       ],
+
                     );
                   },
                 );
